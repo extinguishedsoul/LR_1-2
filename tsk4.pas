089c50df430aca644a87bcf0699b6caaec71fb2d
extinguishedsoul <vilordimarianna343@gmail.com>
2024-09-30 18:16:32 +0300

    Add files via upload

---
+++ tsk4.pas
@@ -0,0 +1,14 @@
+﻿program bnld;
+var x1, x2, y1, y2: integer;
+ begin
+ writeLn('Введите координаты ладьи (пример: A1):');
+ readln(x1);
+ readln(y1);
+ writeln('Введите координаты другой фигуры (пример: A1):');
+ readln(x2);
+ readln(y2);
+ if (x1=x2) or (y1=y2) then
+   writeln('Ладья бьет фигуру')
+ else
+   writeln('Ладья не бьет фигуру');
+end.
