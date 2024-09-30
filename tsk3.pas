6515e98270306990b61c43e5042577f6acc045fa
extinguishedsoul <vilordimarianna343@gmail.com>
2024-09-30 18:16:12 +0300

    Add files via upload

---
+++ tsk3.pas
@@ -0,0 +1,12 @@
+﻿program smnnmrv;
+var nmr1, nmr2: real;
+begin
+  write('Введите первое число: ');
+  readln(nmr1);
+  write('Введите второе число: ');
+  readln(nmr2);
+  if nmr1 <= nmr2 then
+    nmr1 := 0;
+  writeln('Первое число: ', nmr1);
+  writeln('Второе число: ', nmr2);
+end.
